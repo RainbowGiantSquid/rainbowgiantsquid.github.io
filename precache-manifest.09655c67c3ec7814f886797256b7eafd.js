494b0e8ad7000ed5ded1fd92847a297674a067ac
Dagmara <dagmarakukla@gmail.com>
2019-05-25 14:13:20 +0100

    Updates

---
+++ precache-manifest.09655c67c3ec7814f886797256b7eafd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "490bf10facc050633a00fa88a0e6d3a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "071292b3700c463787fa",
+    "url": "/static/css/main.85462a60.chunk.css"
+  },
+  {
+    "revision": "e23023a02da58184ca26",
+    "url": "/static/js/2.99d0534a.chunk.js"
+  },
+  {
+    "revision": "071292b3700c463787fa",
+    "url": "/static/js/main.1578d174.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
